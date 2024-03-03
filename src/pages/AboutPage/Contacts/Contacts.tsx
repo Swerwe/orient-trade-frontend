6366b092f08bc2e43bff4d277dfbc7e958d8428c
Александр <aleksandr@MacBook-Air-Aleksandr.local>
2024-03-03 20:54:08 +0300

    Changed navbar font sizes, added links to slider

--- src/pages/AboutPage/Contacts/Contacts.tsx
+++ src/pages/AboutPage/Contacts/Contacts.tsx
@@ -15,13 +15,13 @@ export const Contacts: React.FC = () => {
         <div className="header2">Мы в социальных сетях:</div>
         <div className="icons">
             <a href='https://t.me/Japcar25'>
-                <TelegramIcon color='#f5914f' height={50} width={50} />
+                <TelegramIcon height={50} width={50} />
             </a>
             <a href='https://wa.me/79841429456?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%21%20%D0%98%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20%D1%86%D0%B5%D0%BD%D0%B0%20%D0%BD%D0%B0%20%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C%3A'>
-                <WhatsappIcon color='#f5914f' height={50} width={50} />
+                <WhatsappIcon height={50} width={50} />
             </a>
             <a href='https://www.instagram.com/japcar25'>
-                <InstagramIcon color='#f5914f' height={50} width={50} />
+                <InstagramIcon height={50} width={50} />
             </a>
 
         </div>
