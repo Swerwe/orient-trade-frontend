643d2b790f1684ec156d39a7bcf25af7f0f5f03c
Alexander Privezentsev <aleksandr@MacBook-Air-Aleksandr.local>
2024-07-08 18:59:33 +0300

    Orient Trade -> Orient Car

--- src/widgets/NavBar/CompanyName/CompanyName.tsx
+++ src/widgets/NavBar/CompanyName/CompanyName.tsx
@@ -5,7 +5,7 @@ export const CompanyName: React.FC = () => {
       <div className="CompanyName">
         <img src='/whitelogo.webp'></img>
         <div>
-          <div>ORIENT TRADE</div>
+          <div>ORIENT CAR</div>
           <a href='tel:+79841429456'><div>+79841429456</div></a>
         </div>
 
